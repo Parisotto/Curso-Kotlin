efb7a0b2da5997ffcbf41eefda001440190c1b19
Edson Parisotto <parisotto@gmail.com>
2024-06-28 14:49:47 -0300

    Create a002-operadores-aritmeticos.kt

---
+++ a002-operadores-aritmeticos.kt
@@ -0,0 +1,25 @@
+// OPERADORES ARITMÉTICOS: +, -, *, /, %
+fun main(){
+    var pontos = 15
+
+//    pontos = pontos + 1
+//    pontos = pontos + 2
+//    pontos = pontos / 3
+//    pontos = pontos * 2
+//    pontos = pontos % 4
+
+//    pontos += 1
+//    pontos -= 1
+//    pontos++
+//    pontos--
+//    ++pontos
+//    --pontos
+
+
+    println("Marquei ${pontos++} pontos") // pós incremento
+    println("Marquei ${++pontos} pontos") // pré incremento
+
+    println("Marquei ${pontos--} pontos") // pós decremento
+
+    println("Marquei ${pontos} pontos")
+}
